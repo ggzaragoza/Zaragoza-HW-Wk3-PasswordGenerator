607d2e1088cf46f7f960881b0c5146c7b1d9d914
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-08-14 18:53:17 -0500

    Continued edits to style.css and script.js

--- assets/script.js
+++ assets/script.js
@@ -2,50 +2,25 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function beginPassword() {
 
+  // Asks user for and displays password length, will return alert if input is not a valid password length
+  var askLength = prompt("Please choose the length of your new password by entering a number value between 8 and 128. Passwords can be 8-128 characters long.");
 
-
-  // Asks user for password length, will return alert if input is not a valid password length
-  var askLength = parseInt(prompt("Please choose the length of your new password by entering a number value between 8 and 128. Passwords can be 8-128 characters long."), 10);
-
-  if (askLength < 8 || askLength > 128) {
+  if (askLength < 8 || askLength > 128 || askLength == " ") {
     alert ("Please enter a valid number of characters for your new password.");
-    return writePassword();
-  } else {
+    return beginPassword();
+  } else { 
     alert ("Your new password will be " + askLength + " characters long.");
   }
 
-  // var minPwordLength = 8;
-
-  // var validPasswordLength = [minPwordLength]
-  //   for (var i = 0; i < 121; i++) {
-  //     validPasswordLength.push(i + 8)
-  // }
-    
-  // if (!validPasswordLength.includes(askLength)) {
-  //   var needValidNum = confirm ("Please enter a valid number of characters for your new password.");
-  //   } else {
-  //   alert ("Your new password will be " + askLength + " characters long.");
-  // }
-
-  // if (needValidNum) {
-  //   writePassword();
-  // }
-  debugger
-
-
-
-  // choice interpreted as boolean
+  // Prompts asks user which character( set)s to include when generating password
   var askAlpha = confirm("Would you like to include lowercase characters in your new password?");
-  console.log(askAlpha);
   var askUpper = confirm("Would you like to include uppercase characters in your new password?");
-  console.log(askUpper);
   var askNum = confirm("Would you like to include numeric characters in your new password?");
-  console.log(askNum);
-  var askSpecial = confirm("Would you like to include special characters in your new password?");
-  console.log(askSpecial);
+  var askSpecial = confirm("Would you like to include special characters (!, @, #, $, %, ^, &, _) in your new password?");
 
+  // Users' choices above are stored here as true/false values
   var confirmedPwordCriteria = {
     askLength: askLength,
     askAlpha: askAlpha,
@@ -54,9 +29,10 @@ function writePassword() {
     askSpecial: askSpecial,
   }
 
+  // Users will restart process if no character sets are selected
   if (askAlpha === false && askUpper === false && askNum === false && askSpecial === false) {
     alert("Please select at least one character set to be used for generating your new password.");
-    return writePassword();
+    return beginPassword();
   }
 
   return confirmedPwordCriteria;
@@ -65,97 +41,65 @@ function writePassword() {
 
 
 
+// All available character( set)s are stored here
 var alphaCharacters = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharacters = ["!", "#", "%", "&", "@", "$", "_"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "_"];
 
 
 
-function getCharacterFrom(characterSet) {
-  var randomCharacter = Math.floor(Math.random() * characterSet.length);
-  var result = characterSet[randomCharacter];
-  return result;
-}
-
-
-
+// Based on users' preferences for password generation, chosen character sets will merge into one array thus containing all potential characters for users' passwords 
 function generatePassword() {
-  var initWrite = writePassword();
-  var potentialCharacterSets = [];
-  var chosenCharacters = [];
+
+  var initiate = beginPassword();
+  var potentialCharacters = [];
+  var characterChoices = [];
   var newPassword = [];
 
-  if (initWrite.askAlpha) {
-    potentialCharacterSets = potentialCharacterSets.concat(alphaCharacters);
-    chosenCharacters.push(getCharacterFrom(alphaCharacters));
+  if (initiate.askAlpha) {
+    potentialCharacters = potentialCharacters.concat(alphaCharacters);
   }
-  if (initWrite.askUpper) {
-    potentialCharacterSets = potentialCharacterSets.concat(upperCharacters);
-    chosenCharacters.push(getCharacterFrom(upperCharacters));
+  if (initiate.askUpper) {
+    potentialCharacters = potentialCharacters.concat(upperCharacters);
   }
-  if (initWrite.askNum) {
-    potentialCharacterSets = potentialCharacterSets.concat(numCharacters);
-    chosenCharacters.push(getCharacterFrom(numCharacters));
+  if (initiate.askNum) {
+    potentialCharacters = potentialCharacters.concat(numCharacters);
   }
-  if (initWrite.askSpecial) {
-    potentialCharacterSets = potentialCharacterSets.concat(specialCharacters);
-    chosenCharacters.push(getCharacterFrom(specialCharacters));
+  if (initiate.askSpecial) {
+    potentialCharacters = potentialCharacters.concat(specialCharacters);
   }
 
-  for (var i = 0; i < initWrite.askLength; i++) {
-    var characterChoices = getCharacterFrom(potentialCharacterSets);
+  // Retrieves random character from newly merged array of potential characters
+  function getCharacterFrom() {
+    var randIdx = Math.floor(Math.random() * potentialCharacters.length);
+    var result = potentialCharacters[randIdx];
+    return result;
+  }
+
+  // Loop will iterate function to retrieve characters for password as necessary to satisfy password length based on users' inputs
+  // Retrieved random characters are placed into new array
+  for (var i = 0; i < initiate.askLength; i++) {
+    var characterChoices = getCharacterFrom(potentialCharacters);
     newPassword.push(characterChoices);
     }
-
-  for (var i = 0; i < chosenCharacters.length; i++) {
-    newPassword[i] = chosenCharacters[i];
-    }
   
+  // Joins random characters into final password
   return newPassword.join("");
+
 }
 
 
-
-
+// Retrieves newly formed string of random characters and places string into #password text area within HTML
 function passwordGenerator() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
 }
 
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", passwordGenerator);
-
-
-
-
-
-
-
-// function randomCharacterSelection() {
-
-//   var alphaChoice = function() {
-//     return alphaCharacters[Math.floor(Math.random() * 26)];
-//   }
-//   console.log(alphaChoice());
-//   var upperChoice = function() {
-//     return upperCharacters[Math.floor(Math.random() * 26)];
-//   }
-//   console.log(upperChoice());
-//   var numChoice = function() {
-//     return numCharacters[Math.floor(Math.random() * 10)];
-//   }
-//   console.log(numChoice());
-//   var specialChoice = function() {
-//     return specialCharacters[Math.floor(Math.random() * 7)];
-//   }
-//   console.log(specialChoice());
-
-// } 
-
-
-
-
-
